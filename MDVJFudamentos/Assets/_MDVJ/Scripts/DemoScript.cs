b8632ab9030e5f300ffdff47b8dfa6334fb014d3
Marco Garcia <86928162+hiddenDevXR@users.noreply.github.com>
2023-09-25 13:24:28 +0100

    Initial commit
    First commit with base project and some third party assets.

---
+++ MDVJFudamentos/Assets/_MDVJ/Scripts/DemoScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am starting!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("I am updating...");
+    }
+}
